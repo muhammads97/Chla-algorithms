fc886cd94df0b71669c4795c906603440852e4fe
Muhammad Salah <msalah.29.10@gmail.com>
2024-01-01 15:24:48 +0900

    add missing file headers

--- src/config.py
+++ src/config.py
@@ -1,3 +1,12 @@
+#
+# Copyright (c) 2024 Muhammad Salah msalah.29.10@gmail.com
+# Licensed under AGPL-3.0-or-later.
+# Refer to LICENSE for the AGPL license.
+# All rights reserved.
+# This project is developed as part of my research in the Remote Sensing Laboratory
+# in Kyoto University of Advanced Science towards my Master's Degree course.
+#
+
 from enum import Enum
 class SENSORS(Enum):
     SGLI = "SGLI"
